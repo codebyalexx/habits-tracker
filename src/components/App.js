5d82817e0706f81a16f87c53215340305bd2f794
AlexandreLG2022 <a.legal.contact@gmail.com>
2023-12-22 20:14:19 +0100

    :building_construction: Created project base structure

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className='container mx-auto py-6'>
+      <h1 className='text-4xl font-bold'>
+        Hello, world!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
