74f85c631d6b674d283b950f4cfab6aed28c9aba
AlexandreLG2022 <a.legal.contact@gmail.com>
2023-12-23 09:52:35 +0100

    :sparkles: Created redux store

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,15 @@
+import store from "../utils/store";
+import {Provider as StoreProvider} from "react-redux";
+
 function App() {
   return (
-    <div className='container mx-auto py-6'>
-      <h1 className='text-4xl font-bold'>
-        Hello, world!
-      </h1>
-    </div>
+    <StoreProvider store={store}>
+        <div className='container mx-auto py-6'>
+            <h1 className='text-4xl font-bold'>
+                Hello, world!
+            </h1>
+        </div>
+    </StoreProvider>
   );
 }
 
